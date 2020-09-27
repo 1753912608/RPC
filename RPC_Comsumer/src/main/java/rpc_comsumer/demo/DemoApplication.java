18a1b0d9fd9809308d161e02f6c91e95ab067727
茗少 <1753972608@qq.com>
2020-09-27 23:04:32 +0800

    细节修改

---
+++ RPC_Comsumer/src/main/java/rpc_comsumer/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package rpc_comsumer.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
---
+++ RPC_Provider/src/test/java/rpc_provider/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package rpc_provider.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
